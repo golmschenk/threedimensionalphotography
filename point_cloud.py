faccd3ab41edf54c8cd4770244d83d86f435c594
Greg Olmschenk <golmschenk@gmail.com>
2015-03-06 11:34:25 -0500

    Point cloud creation

---
+++ point_cloud.py
@@ -0,0 +1,11 @@
+"""
+Point cloud stuff.
+"""
+
+class PointCloud:
+    """
+    Class for the point cloud.
+    """
+
+    def __init__(self, *args):
+        self.points = args
--- tests/test_point_cloud.py
+++ tests/test_point_cloud.py
@@ -2,8 +2,27 @@
 Tests for the PointCloud class.
 """
 
+from ..point_cloud import PointCloud
+from ..point import Point
+
+
 class TestPointCloud:
     """
     Class of tests for PointCloud.
     """
-    pass
+    def test_creation(self):
+        """
+        Test that a point cloud can be created.
+        """
+        point_cloud = PointCloud()
+
+        assert type(point_cloud) is PointCloud
+
+    def test_setting_points_on_creation(self):
+        """Test coordinates can be passed in during creation."""
+        point0 = Point(1, 2, 3)
+        point1 = Point(4, 5, 6)
+        point_cloud = PointCloud(point0, point1)
+
+        assert point0 in point_cloud.points
+        assert point1 in point_cloud.points
