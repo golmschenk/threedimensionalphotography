5020499620bb16918313ebc41564dbda5c84cad2
Greg Olmschenk <golmschenk@gmail.com>
2015-03-09 10:46:26 -0400

    Rename close files

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from point_cloud import PointCloud
 from point import Point
 from ptx_handler import PtxHandler
 
-point_cloud0 = PtxHandler.point_cloud_from_ptx("gh17to16.ptx")
+point_cloud0 = PtxHandler.point_cloud_from_ptx("gh17to16close.ptx")
 point_cloud1 = PtxHandler.point_cloud_from_ptx("gh16.ptx")
 point_cloud0.icp_iteration(point_cloud1, 1000, 0.1)
 PtxHandler.ptx_from_point_cloud("test.ptx", point_cloud1)
